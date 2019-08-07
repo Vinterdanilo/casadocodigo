74fe8cfbfe151e704a1a86a535bf7eac2ab5af74
Danilo Vinter <danilo.vinter@hotmail.com>
2019-08-07 10:26:57 -0300

    Criando Validacoes de Objetos 'Produto'

--- src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
@@ -2,15 +2,22 @@ package br.com.casadocodigo.loja.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
 import br.com.casadocodigo.loja.model.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produto")
@@ -18,8 +25,13 @@ public class ProdutoController {
 
 	//CDI
 	@Autowired private ProdutoDAO produtoDAO;
+	//fazendo ligacao do @Valid com o ProdutoValidator
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
 	
-	@RequestMapping("/form")
+	@RequestMapping("form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produto/form");
 		//retornando a lista de tipos de precos do objeto Preco;
@@ -28,10 +40,17 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String adicionar(Produto produto) {
+	public ModelAndView adicionar(@Valid Produto produto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return form();
+		}
+		
 		System.out.println(produto);
 		produtoDAO.adicionar(produto);
-		return "produto/ok";
+
+		redirectAttributes.addFlashAttribute("mensagem", "Produto adicionado com sucesso!");
+		return new ModelAndView("redirect: produto");
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
--- src/main/webapp/WEB-INF/views/produto/lista.jsp
+++ src/main/webapp/WEB-INF/views/produto/lista.jsp
@@ -1,30 +1,45 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>Lista de Produtos</h1>
-	
-	<div>${sucesso }</div>
-	
-	<table>
-		<tr>
-			<td>Título</td>
-			<td>Descrição</td>
-			<td>Páginas</td>
-		</tr>
-		<c:forEach items="${produtos }" var="produto">
-			<tr>
-				<td>${produto.titulo }</td>
-				<td>${produto.descricao }</td>
-				<td>${produto.paginas }</td>
-			</tr>
-		</c:forEach>
-	</table>
+	<div class="container">
+		<br>
+		<h3>Listagem de Produtos</h3>
+		<br>
+		
+		<div>${mensagem}</div>
+
+		<table
+			class="table table-striped table-borderedtable-hover  table-sm table-condensed border-dark table-light">
+			<thead class="thead-dark">
+				<tr>
+					<td><strong>ID</strong></td>
+					<td>Título</td>
+					<td>Descrição</td>
+					<td>Páginas</td>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach items="${produtos}" var="produto">
+					<tr>
+						<td><strong>${produto.id_produto}</strong></td>
+						<td>${produto.titulo}</td>
+						<td>${produto.descricao}</td>
+						<td>${produto.paginas}</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
